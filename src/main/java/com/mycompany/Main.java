b77d3fec0654159046e63b4a783a1026c00c1dfb
gitpawelo <gitpawelo@wp.pl>
2019-09-20 21:18:58 +0200

    add movies count and year interval

--- src/main/java/com/mycompany/Main.java
+++ src/main/java/com/mycompany/Main.java
@@ -10,6 +10,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 public class Main {
@@ -19,7 +20,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         try (
-            Reader reader = Files.newBufferedReader(Paths.get(ClassLoader.getSystemResource(CSV_FILE_PATH).toURI()));
+            Reader reader = Files.newBufferedReader(Paths.get(ClassLoader.getSystemResource(CSV_FILE_PATH).toURI()))
         ) {
             CsvToBean<Movie> csvBean = new CsvToBeanBuilder(reader)
                     .withType(Movie.class)
@@ -27,6 +28,29 @@ public class Main {
                     .build();
 
             List<Movie> movies = csvBean.parse();
+
+
+            for (Movie moviesList: movies
+                 ) {
+                System.out.println("Id filmu: " + moviesList.getMovieID());
+                System.out.println("Tytuł: " + moviesList.getTitle());
+                System.out.println("Rok produkcji: " + moviesList.getYear());
+                System.out.println("Typ filmu: " + moviesList.getGenre());
+                System.out.println("========================================");
+
+            }
+
+            Long moviesVolume = movies.stream()
+                    .map(moviesID -> moviesID.getMovieID())
+                    .count();
+            System.out.println("Liczba filmów: " + moviesVolume);
+
+            IntSummaryStatistics statistics = movies.stream()
+                    .mapToInt(moviesYear -> moviesYear.getYear())
+                    .summaryStatistics();
+            System.out.println("Przedział czasowy filmów to: " + statistics.getMin() + "-" + statistics.getMax());
+
+
 
         }catch (URISyntaxException ex){
             ex.printStackTrace();
